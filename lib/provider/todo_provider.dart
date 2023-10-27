b535f30a413a0b6469aa5ac2b95feeea0e62c7e5
sanju518 <sanjukushwaha5518@gmail.com>
2023-10-27 14:24:16 +0545

    changes

--- lib/provider/todo_provider.dart
+++ lib/provider/todo_provider.dart
@@ -3,7 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../models/todo.dart';
 
-final todoProvider = StateNotifierProvider<TodoProvider, List<Todo>>((ref) => TodoProvider([]));
+final todoProvider = StateNotifierProvider<TodoProvider, List<Todo>>((ref) => TodoProvider([
+  Todo(DateTime: DateTime.now().toString(), todo: 'This is my task'),
+  Todo(DateTime: DateTime.now().toString(), todo: 'This is my task on Staring'),
+
+]));
 
 
 class TodoProvider extends StateNotifier<List<Todo>>{
